a86f4e171250bbd9348b4b4d2852a87999a7bbf1
Wxrst <telmoparis250@gmail.com>
2024-02-04 18:26:09 +0000

    atualização do site

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,15 +1,33 @@
 /*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
+const showMenu = (toggleId, navId, iconId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+    nav = document.getElementById(navId),
+    icon = document.getElementById(iconId);
 
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
+            nav.classList.toggle('show');
+            icon.classList.toggle('bx-x'); // Adiciona ou remove a classe 'bx-x'
         })
     }
+
+    // Fecha o menu quando se clica fora dele
+    document.addEventListener('click', (e) => {
+        if(e.target.closest(`#${navId}`) || e.target.closest(`#${toggleId}`)) {
+            return;
+        }
+        if(nav.classList.contains('show')) {
+            nav.classList.remove('show');
+            icon.classList.remove('bx-x');
+            icon.classList.add('bx-menu');
+            icon.style.transform = "rotate(0deg)";
+        }
+    });
 }
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle','nav-menu', 'menu-icon');
+
+
+
 
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
@@ -203,3 +221,26 @@ function toggleTheme() {
     changeIcon();
   });
   
+  // Seleciona todos os cartões
+const cards = document.querySelectorAll('.clip-card');
+
+cards.forEach(card => {
+    // Adiciona um ouvinte de eventos de clique a cada cartão
+    card.addEventListener('click', (e) => {
+        // Verifica se o overlay já está mostrando
+        const overlayShowing = card.classList.contains('show-overlay');
+
+        // Remove a classe 'show-overlay' de todos os cartões
+        cards.forEach(card => card.classList.remove('show-overlay'));
+
+        // Se o overlay não estava mostrando, adiciona a classe 'show-overlay'
+        if (!overlayShowing) {
+            card.classList.add('show-overlay');
+        }
+
+        // Previne o evento de clique se o overlay não estava mostrando
+        if (!overlayShowing && e.target.tagName === 'A') {
+            e.preventDefault();
+        }
+    });
+});
