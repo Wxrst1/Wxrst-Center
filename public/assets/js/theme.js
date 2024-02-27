be73c72c6e570ccaa4611d68319d28a07b03e06e
Wxrst <telmoparis250@gmail.com>
2024-02-27 01:12:40 +0000

    otimização

--- public/assets/js/theme.js
+++ public/assets/js/theme.js
@@ -1,4 +1,3 @@
-// Função para alternar o tema
 function toggleTheme() {
   const body = document.body;
   body.classList.toggle("dark-theme");
@@ -7,16 +6,11 @@ function toggleTheme() {
   if (socialElement) {
     socialElement.classList.toggle("dark-theme");
   }
-  // Atualiza as cores atuais com base no tema atual
   const isDarkTheme = body.classList.contains("dark-theme");
   document.documentElement.style.setProperty("--primary-color", isDarkTheme ? "var(--primary-color-dark)" : "var(--primary-color-light)");
   document.documentElement.style.setProperty("--secondary-color", isDarkTheme ? "var(--secondary-color-dark)" : "var(--secondary-color-light)");
-
-  // Salva a preferência de tema no localStorage
   localStorage.setItem('theme', isDarkTheme ? 'dark' : 'light');
 }
-
-// Função para mudar o ícone
 function changeIcon() {
   var button = document.getElementById('theme-button');
   var icon = document.getElementById('theme-icon');
@@ -31,20 +25,14 @@ function changeIcon() {
 
   }
 }
-// Evento de clique no botão de alternar o tema
 const themeButton = document.getElementById("theme-button");
 themeButton.addEventListener("click", function() {
   toggleTheme();
   changeIcon()
 });
-
-// Quando a página é carregada...
 document.addEventListener('DOMContentLoaded', function() {
-  // Obtém a preferência de tema do localStorage
   var theme = localStorage.getItem('theme');
-
   if (theme) {
-    // Se o tema salvo for 'dark'...
     if (theme === 'dark') {
         // Ativa o tema escuro
         document.body.classList.add("dark-theme");
@@ -57,6 +45,5 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('theme-icon').classList.add('bxs-moon');
     }
 }
-  // Chama a função changeIcon para garantir que o ícone esteja correto
   changeIcon();
 });
